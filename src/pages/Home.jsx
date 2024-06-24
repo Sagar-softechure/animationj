4e5c6417f40aa559f08da3528ef63f4b220bde0b
Sagar-softechure <136797867+Sagar-softechure@users.noreply.github.com>
2024-06-24 11:03:13 +0530

    sagar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,23 +3,10 @@ import gsap from "gsap-trial";
 import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
 import { ScrollTrigger } from "gsap-trial/ScrollTrigger";
 
-<<<<<<< HEAD
-    return(
-        <>  
-        <section style={{height:"100vh"}}>
-            <span className="sub-head">Split Text </span>
-            <h1>
-                Home
-            </h1>
-        </section>
-        </>
-    );
-=======
+
 gsap.registerPlugin(ScrollSmoother,ScrollTrigger);
 const Home = () => {
->>>>>>> e2ba756260fc3a264e81a2eefecd3a3112d7a54b
 
-   
       
   return (
     <>
